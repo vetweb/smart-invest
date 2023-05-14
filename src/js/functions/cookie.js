3f3ae5727c6dc8224d23423fdb4ac9eae8b59293
Vitali <v.matyunin@jetmail.cc>
2023-05-14 12:39:00 +0300

    cookie added

--- src/js/functions/cookie.js
+++ src/js/functions/cookie.js
@@ -2,10 +2,37 @@
 	const cookieBtn = document.querySelector('.js-cookie-btn-trigger');
 	const cookieContainer = document.querySelector('.js-cookie-popup');
 
-	if(cookieBtn) {
-		cookieBtn.addEventListener('click', () => {
-			cookieContainer.classList.add('hide');
-		})
+	function setCookie(name, value, days) {
+		let expires = "";
+		if (days) {
+			let date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toUTCString();
+		}
+		document.cookie = name + "=" + (value || "") + expires + "; path=/";
 	}
+
+	function getCookie(name) {
+		let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+
+
+	function checkCookies() {
+		// Если куки cookies_policy нет или она просрочена, то показываем уведомление
+		if (!getCookie('cookies_policy')) {
+			cookieContainer.classList.add('show');
+		}
+
+		// При клике на кнопку устанавливаем куку cookies_policy на один год
+		cookieBtn.addEventListener('click', function () {
+			setCookie('cookies_policy', 'true', 365);
+			cookieContainer.classList.remove('show');
+			cookieContainer.classList.add('hide');
+			console.log(1)
+		});
+	}
+
+	checkCookies();
 
 })();
